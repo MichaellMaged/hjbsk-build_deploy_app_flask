92e0d2cd163a584748a3c4156624f78972b59fe4
MichaellMaged <michaelmaged19@gmail.com>
2023-08-17 12:44:45 -0400

    Merging All changes
    Signed-off-by: MichaellMaged <michaelmaged19@gmail.com>

--- server.py
+++ server.py
@@ -1,32 +1,22 @@
 from flask import Flask, render_template, request
-# Import the Maths package here
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-app = Flask("Mathematics Problem Solver")
+app = Flask("Sentiment Analyzer")
 
-@app.route("/sum")
-def sum_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    result=summation(num1,num2)
-    return str(result)
-
-@app.route("/sub")
-def sub_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    result=subtraction(num1,num2)
-    return str(result)
-
-@app.route("/mul")
-def mul_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    result=multiplication(num1,num2)
-    return str(result) 
+@app.route("/sentimentAnalyzer")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = sentiment_analyzer(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    if label is None:
+        return "Invalid input ! Try again."
+    else:
+        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
